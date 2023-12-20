1a32692593349075ec27e959736bb2662ef703b9
javierpardollama <javierpardollama@outlook.com>
2023-12-20 09:40:06 +0100

    Control de Salida de Sistema

--- domain/nucleo.py
+++ domain/nucleo.py
@@ -12,28 +12,31 @@ class Nucleo:
     def __start__(self):
         opcion: str = ""
 
-        while opcion not in ["1", "2", "3", "4"]:
-            opcion = (input("\n" +
-                            "************************************** \n" +
-                            "************* ¿QUE JUEGO? ************ \n" +
-                            "1. TRES EN RAYA \n" +
-                            "2. MAZMORRAS & DRAGONES \n" +
-                            "3. JUEGO DE LA VIDA \n" +
-                            "4. SALIR \n" +
-                            "************************************** \n")).upper()
+        try:
+            while opcion not in ["1", "2", "3", "4"]:
+                opcion = (input("\n" +
+                                "************************************** \n" +
+                                "************* ¿QUE JUEGO? ************ \n" +
+                                "1. TRES EN RAYA \n" +
+                                "2. MAZMORRAS & DRAGONES \n" +
+                                "3. JUEGO DE LA VIDA \n" +
+                                "4. SALIR \n" +
+                                "************************************** \n")).upper()
 
-            match opcion:
-                case "1":
-                    self.tiktak.__start__()
-                    self.__start__()
-                case "2":
-                  self.dungeon.__start__()
-                  self.__start__()
-                case "3":
-                    self.conway.__start__()
-                    self.__start__()
-                case "4":
-                    self.stop()
+                match opcion:
+                    case "1":
+                        self.tiktak.__start__()
+                    case "2":
+                      self.dungeon.__start__()
+                    case "3":
+                        self.conway.__start__()
+                    case "4":
+                        self.stop()
+
+        except SystemExit:
+            self.__start__()
+        finally:
+            self.stop()
 
     def stop(self) -> None:
         quit()
